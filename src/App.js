0e77aa2d035228edd21e59095de06ab3b72dd43c
Vidhi <vidhigupta00003@gmail.com>
2023-09-05 13:13:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import './Vidhi.css';
+import Button from './Button'; 
 
 function App() {
   return (
-    <div className="App">
+    <div className="First">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>hello world</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Button.css';
+
+ const Button = () => {
+  return (
+    <div className="button">
+        <button type='button'>Click Here</button>
+    </div>
+  );
+};
+export default Button;
